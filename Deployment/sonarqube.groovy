67a50ac3de2c3ca9940ac20036091894dabb6f73
vivien.s.chen <vivien.chen@plus3it.com>
2018-12-31 17:26:44 -0500

    Added sonar-project.properties file

--- Deployment/sonarqube.groovy
+++ Deployment/sonarqube.groovy
@@ -40,9 +40,6 @@ pipeline {
 				withSonarQubeEnv('Sonar') {
 					sh "${scannerHome}/bin/sonar-scanner"
 				}
-				timeout(time: 10, unit: 'MINUTES') {
-					waitForQualityGate abortPipeline: true
-				}
 			}
 		}
 	}

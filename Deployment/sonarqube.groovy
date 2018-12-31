a48e2dda9185a851c86205d59a03b73a71d49f66
vivien.s.chen <vivien.chen@plus3it.com>
2018-12-31 16:17:48 -0500

    New files for configuring jenkin pipeline job

---
+++ Deployment/sonarqube.groovy
@@ -0,0 +1,42 @@
+
+pipeline {
+
+    agent any
+
+    options {
+        buildDiscarder(
+            logRotator(
+                numToKeepStr: '5',
+                daysToKeepStr: '30',
+                artifactDaysToKeepStr: '30',
+                artifactNumToKeepStr: '3'
+            )
+        )
+        disableConcurrentBuilds()
+        timeout(time: 60, unit: 'MINUTES')
+    }
+
+    stages {
+        stage ('Prepare Instance Environment') {
+            steps {
+                deleteDir()
+                git branch: "${GitProjBranch}",
+                    credentialsId: "${GitCred}",
+                    url: "${GitProjUrl}
+			}
+		}
+		stage('Sonarqube') {
+			environment {
+				scannerHome = tool 'SonarQubeScanner'
+			}
+			steps {
+				withSonarQubeEnv('sonarqube') {
+					sh "${scannerHome}/bin/sonar-scanner"
+				}
+				timeout(time: 10, unit: 'MINUTES') {
+					waitForQualityGate abortPipeline: true
+				}
+			}
+		}
+	}
+}
